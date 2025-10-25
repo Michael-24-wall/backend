e25e316a38b017435e0918b0ae8fa171763d2035
Michael-24-wall <wallancemichael17@gmail.com>
2025-10-25 14:06:40 +0300

    update settings

--- paperless_saas/settings.py
+++ paperless_saas/settings.py
@@ -221,6 +221,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:9000",  # Add your backend port too
+    "http://127.0.0.1:9000",
 ]
 
 # Or if you want to keep it simple during development:
